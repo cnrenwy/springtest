987507b01173a27eadef80a8fa81f92b8cb4ad0d
cnrenwy <5887814@qq.com>
2019-04-07 11:16:12 +0800

    first commit

---
+++ src/main/java/com/ruolan/springtest/HelloController.java
@@ -0,0 +1,14 @@
+package com.ruolan.springtest;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableAutoConfiguration
+public class HelloController {
+    @RequestMapping("/hello")
+    private String index(){
+        return "hello world，this is ok1";
+    }
+}
